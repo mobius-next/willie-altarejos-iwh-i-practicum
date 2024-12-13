a045f58008eddc0ab6e9f3b4a376464c7825213a
Willie Jan Altarejos <williejan@mobiusnext.com>
2024-12-13 19:53:37 +0800

    update index.js with externalization

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
+
 const app = express();
+
+const PORT = process.env.PORT || 3000; 
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,15 +13,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.APIKEY;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+// Call api for Technologies custom object
+app.get('/technologies', async (req, res) => {
+    const technologies = 'https://api.hubspot.com/crm/v3/objects/2-38101974';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        res.render('technologies', { title: 'Technologies | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
